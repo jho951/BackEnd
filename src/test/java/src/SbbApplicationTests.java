97e606ddc51bed5b16ca9bf6e11bf6946ba504d7
jhon S <jhons@jhonui-MacBookPro-2.local>
2025-04-22 14:33:57 +0900

    chore: security.md 추가

--- src/test/java/src/SbbApplicationTests.java
+++ src/test/java/src/SbbApplicationTests.java
@@ -1,31 +1,8 @@
 package src;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.List;
 
-import src.domain.sample.entity.Sample;
-import src.domain.sample.repository.SampleRepository;
 
 @SpringBootTest
 class SbbApplicationTests {
-	@Autowired
-	private SampleRepository sampleRepository;
-
-	@Test
-	void testHello(){
-		Sample sample = new Sample();
-		Sample.builder()
-			.name("이장호")
-			.build();
-		sampleRepository.save(sample);
-	}
-
-	@Test
-	void testFindSize() {
-		List<Sample> sampleList = sampleRepository.findAll();
-		assertEquals(1, sampleList.size());
-	}
 }
