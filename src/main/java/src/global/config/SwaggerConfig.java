ffb5cbb8404f3add4364a9d13692126c6cd09a19
jhon S <jhons@jhonui-MacBookPro-2.local>
2025-04-22 14:27:30 +0900

    chore: .github

--- src/main/java/src/global/config/SwaggerConfig.java
+++ src/main/java/src/global/config/SwaggerConfig.java
@@ -3,24 +3,27 @@ package src.global.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springdoc.core.models.GroupedOpenApi;
 
 @Configuration
 public class SwaggerConfig {
 
+	// OpenAPI 기본 정보 설정
 	@Bean
 	public OpenAPI customOpenAPI() {
 		return new OpenAPI()
-			.info(new Info().title("BackEnd").version("v1").description("API Documentation for my Spring Boot app"));
+			.info(new Info().title("BackEnd")
+				.version("v1")
+				.description("API Documentation for my Spring Boot app"));
 	}
 
 	@Bean
 	public GroupedOpenApi publicApi() {
 		return GroupedOpenApi.builder()
 			.group("public")
-			.pathsToMatch("/v1/sample/**", "/logback/**")
+			.pathsToMatch("/v1/sample/**")
 			.build();
 	}
 }
